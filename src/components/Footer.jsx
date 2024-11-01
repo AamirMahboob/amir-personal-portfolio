e96bb2ae7492b236dfaff5982d1c1c9782f25161
Aamir Mahboob <amirmehboob16@gmail.com>
2024-11-02 00:02:28 +0500

    design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,11 @@ import EmailIcon from '@mui/icons-material/Email';
  
 import { Tooltip, Snackbar } from '@mui/material';
 import FooterBottom from "./FooterBottom";
+import { Margin } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 const Footer = () => {
     const theme = useTheme()
-    const email = "reachsagarshah@gmail.com";
+    const email = "amirmehboob16@gmail.com";
     const handleCopy = () => {
       navigator.clipboard.writeText(email);
       alert("Email copied to clipboard!");
@@ -19,47 +21,49 @@ const Footer = () => {
   return (
     <>
     <Container
-      maxWidth="lg"
+      maxWidth="xxl"
       sx={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        padding: "20px",
+        paddingY: "60px",
         // backgroundColor:theme.palette.primary.main
         backgroundColor:'black'
+        
       }}
     >
-      <Box sx={{ padding: "10px",display:'flex',flexDirection:'column',justifyContent:"center",alignItems:'center',width:'50%' }}>
+      <Box sx={{ padding: "10px",display:'flex',flexDirection:'column',justifyContent:"center",alignItems:'center',width:'50%',gap:"10px" }}>
       <CustomTypo variant="h6" sx={{backgroundColor:"#374151",textAlign:'center',paddingX:'20px',paddingY:'6px',borderRadius:'20px'} }   color="white">Get in Touch</CustomTypo>
           
           <Stack spacing={1} marginTop={2} >
-          <CustomTypo variant="h5" sx={{textAlign:"center"}}    color="white">
+          <CustomTypo  fontSize={"20px"} fontWeight={400}  sx={{textAlign:"center"}} lineHeight={"28px"}   color="#d1d5db">
           What’s next? Feel free to reach out to me if you are looking for a developer, have a query, or simply want to connect.
           </CustomTypo>
           </Stack>
           <Stack marginTop={3} spacing={1}>
-          <CustomTypo variant="h5" sx={{textAlign:"center"}}    color="white">
+          <CustomTypo variant="h2" sx={{textAlign:"center"}}     color="rgb(249, 250, 251)">
           {email}
           <Tooltip title="Copy Email" arrow>
-        <EmailIcon 
-          sx={{ fontSize: 40, color: '#333', cursor: 'pointer' }} 
+        {/* <EmailIcon 
+          sx={{ fontSize: 40, color: '#333', cursor: 'pointer' ,}} 
           onClick={handleCopy} 
-        />
+          
+        /> */}
       </Tooltip>
           </CustomTypo>
-          <CustomTypo variant="h5" sx={{textAlign:"center"}}    color="white">
+          <CustomTypo variant="h2" sx={{textAlign:"center"}}    color="white">
           +92 3159142436
           </CustomTypo>
           </Stack>
           <Stack marginTop={3} spacing={1}>
-          <CustomTypo variant="h5" sx={{textAlign:"center"}}    color="white">
+          <CustomTypo variant="h5" fontWidth={'400px'} sx={{textAlign:"center"}}    color="#d1d5db">
             you may also find me on these platform
              
           </CustomTypo>
          <CustomTypo sx={{textAlign:"center", marginTop:'10px'}} >
-          <LinkedInIcon sx={{ fontSize: 40, color: '#333' }} />
+          <a href="https://www.linkedin.com/in/syed-aamir-shah-7322ba1ab/" target="_blank" rel="noopener noreferrer"><LinkedInIcon  sx={{ fontSize: 40, color: '#333' }} /></a>
           <FacebookIcon style={{ fontSize: 40, color: '#1877F2' }} />
-          <GitHubIcon style={{ fontSize: 40, color: '#333' }} />
+          <a href="https://github.com/AamirMahboob" target="_blank"><GitHubIcon style={{ fontSize: 40, color: '#333' }} /></a>
          </CustomTypo>
           </Stack>
           
