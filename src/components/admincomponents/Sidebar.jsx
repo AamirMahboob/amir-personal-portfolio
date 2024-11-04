8dfe00e5e8694b9d32eec19a1060a93e3cf6bdd1
Aamir Mahboob <amirmehboob16@gmail.com>
2024-11-04 23:24:44 +0500

    side

--- src/components/admincomponents/Sidebar.jsx
+++ src/components/admincomponents/Sidebar.jsx
@@ -1,4 +1,3 @@
-// Sidebar.js
 import React from "react";
 import {
   Box,
@@ -10,6 +9,7 @@ import {
   useTheme,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { CustomTypo } from "../common/CustomTypo";
 
 const drawerWidth = 240;
 
@@ -28,20 +28,48 @@ const Sidebar = ({ open, handleDrawerToggle }) => {
           "& .MuiDrawer-paper": {
             width: drawerWidth,
             boxSizing: "border-box",
+            backgroundColor:'#0a101a'
           },
         }}
         open
+        
       >
         <Toolbar />
-        <Box sx={{ overflow: "auto" }}>
-          <List>
-            <ListItem button component={Link} to="dashboard">
-              <ListItemText primary="Dashboard" />
+        <Box sx={{ display: "flex", flexDirection: "column", height: "100%",backgroundColor:'#0a101a' }}>
+          
+          {/* Logo at the Top */}
+          <Box sx={{ p: 2, textAlign: "center",marginTop:'-58px' }}>
+             <CustomTypo color="white">
+             &lt;AM /&gt; 
+             </CustomTypo>
+          </Box>
+          
+          <Box sx={{ flexGrow: 1, overflow: "auto" }}>
+            <List   >
+              <ListItem  button component={Link} to="dashboard">
+                <ListItemText sx={{color:'white'}} primary="Dashboard" />
+              </ListItem>
+              <ListItem  button component={Link} to="hero">
+                <ListItemText  sx={{color:'white'}} primary="Hero" />
+              </ListItem>
+              <ListItem button component={Link} to="admintestimonial">
+                <ListItemText sx={{color:'white'}} primary="Testimonials" />
+              </ListItem>
+              <ListItem button component={Link} to="skills">
+                <ListItemText  sx={{color:'white'}} primary="Skills" />
+              </ListItem>
+              <ListItem button component={Link} to="aboutme">
+                <ListItemText sx={{color:'white'}}  primary="About me" />
+              </ListItem>
+            </List>
+          </Box>
+
+          {/* Logout at the Bottom */}
+          <Box sx={{ mt: "auto", mb: 2 }}>
+            <ListItem button component={Link} to="signin">
+              <ListItemText sx={{color:'white'}} primary="Logout" />
             </ListItem>
-            <ListItem button component={Link} to="settings">
-              <ListItemText primary="Settings" />
-            </ListItem>
-          </List>
+          </Box>
         </Box>
       </Drawer>
 
@@ -62,15 +90,39 @@ const Sidebar = ({ open, handleDrawerToggle }) => {
         }}
       >
         <Toolbar />
-        <Box sx={{ overflow: "auto" }}>
-          <List>
-            <ListItem button component={Link} to="dashboard" onClick={handleDrawerToggle}>
-              <ListItemText primary="Dashboard" />
+        <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+          
+          {/* Logo at the Top */}
+          <Box sx={{ p: 2, textAlign: "center" }}>
+            <img src="/path/to/logo.png" alt="Logo" style={{ width: "80%", height: "auto" }} />
+          </Box>
+          
+          <Box sx={{ flexGrow: 1, overflow: "auto" }}>
+            <List>
+              <ListItem button component={Link} to="dashboard" onClick={handleDrawerToggle}>
+                <ListItemText primary="Dashboard" />
+              </ListItem>
+              <ListItem button component={Link} to="hero" onClick={handleDrawerToggle}>
+                <ListItemText primary="Hero" />
+              </ListItem>
+              <ListItem button component={Link} to="admintestimonial" onClick={handleDrawerToggle}>
+                <ListItemText primary="Testimonials" />
+              </ListItem>
+              <ListItem button component={Link} to="skills" onClick={handleDrawerToggle}>
+                <ListItemText primary="Skills" />
+              </ListItem>
+              <ListItem button component={Link} to="aboutme" onClick={handleDrawerToggle}>
+                <ListItemText primary="About me" />
+              </ListItem>
+            </List>
+          </Box>
+
+          {/* Logout at the Bottom */}
+          <Box sx={{ mb: 2 }}>
+            <ListItem button component={Link} to="signin" onClick={handleDrawerToggle}>
+              <ListItemText primary="Logout" />
             </ListItem>
-            <ListItem button component={Link} to="settings" onClick={handleDrawerToggle}>
-              <ListItemText primary="Settings" />
-            </ListItem>
-          </List>
+          </Box>
         </Box>
       </Drawer>
     </>
