ab5fd1b5818fb679eb19aa4db72c25e8ec52c3ae
Aamir Mahboob <amirmehboob16@gmail.com>
2024-11-06 00:26:25 +0500

    schema commit

--- src/admin/AdminHero.jsx
+++ src/admin/AdminHero.jsx
@@ -1,12 +1,13 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, useTheme, TextField, Button } from "@mui/material";
 import React from "react";
 import { heroSchema } from "../schemas/schemas";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Button, TextField } from "@mui/material";
+import { styled } from "@mui/system";
 
 const AdminHero = () => {
+  const theme = useTheme();
   const initialValue = {
-    title: "",
+    name: "",
     description: "",
     image: "",
     state: "",
@@ -20,69 +21,101 @@ const AdminHero = () => {
         initialValues={initialValue}
         validationSchema={heroSchema}
         onSubmit={(values) => {
-          console.log(values,"ghdcfvgbhnj");
+          console.log(values); // Check console for data here
         }}
       >
         {({ errors, touched, isSubmitting }) => (
           <Form
             style={{
-              display: "flex",
-              flexDirection: "column",
               gap: "1rem",
-              width: "300px",
-              margin: "auto",
+              width: "100%",
+              marginTop: "2rem",
             }}
           >
             <Field
-              as={TextField}
-              label="Title"
-              name="title"
+              as={StyledTextField}
+              label="Name"
+              name="name"
               variant="outlined"
               fullWidth
-              error={touched.title && Boolean(errors.title)}
-              helperText={<ErrorMessage name="title" />}
+              style={{ width: "100%" }}
+              error={touched.name && Boolean(errors.name)}
+              helperText={<ErrorMessage name="name" />}
             />
+
             <Field
-              as={TextField}
-              label="Description"
-              name="description"
-              variant="outlined"
-              fullWidth
-              error={touched.description && Boolean(errors.description)}
-              helperText={<ErrorMessage name="description" />}
-            />
-            {/* <Field
-              as={FileField}
-              label="Image"
+              as={StyledTextField}
+              // label="Upload Image..."
               name="image"
               variant="outlined"
               fullWidth
+              style={{ width: "100%" }}
               error={touched.image && Boolean(errors.image)}
               helperText={<ErrorMessage name="image" />}
-            /> */}
-            <Field
-              as={TextField}
-              label="State"
-              name="state"
-              variant="outlined"
-              fullWidth
-              error={touched.state && Boolean(errors.state)}
-              helperText={<ErrorMessage name="state" />}
+              sx={{
+                marginTop: "1rem",
+              }}
+              type="file" // Change type to file
+              inputProps={{
+                accept: "image/*", // Accept only image files
+              }}
             />
+
+            <Box
+              sx={{
+                display: "flex",
+                gap: 2,
+              }}
+            >
+              <Field
+                as={StyledTextField}
+                label="State"
+                name="state"
+                variant="outlined"
+                fullWidth
+                error={touched.state && Boolean(errors.state)}
+                helperText={<ErrorMessage name="state" />}
+                style={{
+                  marginTop: "1rem",
+                }}
+              />
+              <Field
+                as={StyledTextField}
+                label="Country"
+                name="country"
+                variant="outlined"
+                fullWidth
+                error={touched.country && Boolean(errors.country)}
+                helperText={<ErrorMessage name="country" />}
+                style={{
+                  marginTop: "1rem",
+                }}
+              />
+            </Box>
             <Field
-              as={TextField}
-              label="Country"
-              name="country"
+              as={StyledTextField}
+              label="Description"
+              name="description"
               variant="outlined"
+              multiline
+              minRows={3}
+              maxRows={5}
               fullWidth
-              error={touched.country && Boolean(errors.country)}
-              helperText={<ErrorMessage name="country" />}
+              error={touched.description && Boolean(errors.description)}
+              helperText={<ErrorMessage name="description" />}
+              style={{
+                marginTop: "1rem",
+              }}
             />
             <Button
               type="submit"
               variant="contained"
               color="primary"
               disabled={isSubmitting}
+              sx={{
+                marginTop: "1rem",
+                
+              }}
             >
               Submit
             </Button>
@@ -94,3 +127,26 @@ const AdminHero = () => {
 };
 
 export default AdminHero;
+
+const StyledTextField = styled(TextField)({
+  "& .MuiInputLabel-root": {
+    color: "gray", // Label color
+    
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "blue",  
+      borderRadius: "10px",
+    },
+    "&:hover fieldset": {
+      borderColor: "green", // Hover border color
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "purple", // Focused border color
+    },
+  },
+  "& .MuiInputBase-input": {
+    color: "black", // Text color
+  },
+
+});
